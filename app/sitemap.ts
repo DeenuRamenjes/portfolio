1ec9221d21e0885a1e559f788ccd778623be8c7d
Deenu Ramenjes M S <deenuramenjes29@gmail.com>
2026-02-13 11:09:07 +0530

    build error

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { projects } from '@/lib/projects-data';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://www.deenu.fun';
 
